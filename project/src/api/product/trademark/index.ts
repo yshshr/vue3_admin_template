5f49698ed9b00f4d301216d2df12aa14da5fd221
shaohuyao <1153691789@qq.com>
2024-05-20 22:33:10 +0800

    对话框dialog静态add

--- project/src/api/product/trademark/index.ts
+++ project/src/api/product/trademark/index.ts
@@ -1,9 +1,18 @@
 import request from '@/utils/request';
-import type {GetTradeMarkResponseData} from '@/api/product/trademark/type.ts'
+import type {GetTradeMarkResponseData,record} from '@/api/product/trademark/type.ts'
 
 enum API {
-    GETTRADEMARK_URL='/admin/product/baseTrademark/'
+    GETTRADEMARK_URL='/admin/product/baseTrademark/',
+    ADDTRADEMARK_URL='/admin/product/baseTrademark/save',
+    EDITTRADEMARK_URL='/admin/product/baseTrademark/update'
 }
 
 export const reqGetTradeMark=(page:number,limit:number)=>request.get<any,GetTradeMarkResponseData>(API.GETTRADEMARK_URL+`${page}/${limit}`);
 
+export const reqAddOrUpdateTradeMark=(data:record)=>{
+    if(data.id){
+        request.post<any,any>(API.EDITTRADEMARK_URL,data);
+    }else{
+        request.post<any,any>(API.ADDTRADEMARK_URL,data);
+    }
+}
--- project/src/views/product/trademark/index.vue
+++ project/src/views/product/trademark/index.vue
@@ -1,6 +1,6 @@
 <template>
     <el-card style="width: 100%">
-      <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
+      <el-button type="primary" size="default" icon="Plus" @click="addTradeMark">添加品牌</el-button>
       <el-table :data="tabledata" style="margin:10px 0px" border>
         <el-table-column label="序号" width="80" align="center" type="index" />
         <el-table-column prop="tmName" label="品牌名称" width="180" align="center">
@@ -12,7 +12,7 @@
         </el-table-column>
         <el-table-column prop="op" label="品牌操作" width="180">
           <template #="{ row , $index}">
-            <el-button type="primary" size="small" icon="Edit" >修改</el-button>
+            <el-button type="primary" size="small" icon="Edit" @click="editTradeMark">修改</el-button>
             <el-button type="danger" size="small" icon="Delete" >删除</el-button>
 
           </template>
@@ -29,10 +29,30 @@
       :total="total"
     />
     </el-card>
+    <el-dialog v-model="dialogFormVisible" title="添加品牌" width="500">
+      <el-form style="width:80%" >
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input v-model="dialogFormParams.tmName" placeholder="请输入品牌名称" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px">
+          <el-upload
+            class="avatar-uploader"
+            action="/api/admin/product/fileUpload"
+            :show-file-list="false">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="primary" @click="cancel">取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </template>
+    </el-dialog>
 </template>
 
 <script setup lang=ts>
-import { ref ,onMounted} from 'vue';
+import { ref ,onMounted,reactive} from 'vue';
 import {reqGetTradeMark} from '@/api/product/trademark'
 import type {record,GetTradeMarkResponseData} from '@/api/product/trademark/type.ts'
 
@@ -41,7 +61,15 @@ const pageNo = ref<number>(1)
 const limit = ref<number>(3)
 const total = ref<number>(0)
 const tabledata = ref<record[]>()
-
+const dialogFormVisible = ref<boolean>(false)
+const dialogFormParams = reactive<record>(
+  {
+  tmName:'',
+  logoUrl:'',
+  id:0,
+  createTime: '',
+  updateTime: ''
+})
 
 const getTradeMark = async(pager=1)=> {
   console.log(pager)
@@ -61,8 +89,51 @@ const sizeChange=()=>{
   getTradeMark();
 }
 
+const addTradeMark=()=>{
+  dialogFormVisible.value=true;
+}
+
+const editTradeMark=()=>{
+  dialogFormVisible.value=true;
+}
+
+const cancel=()=>{
+  dialogFormVisible.value=false;
+}
+
+const confirm=()=>{
+  dialogFormVisible.value=false;
+}
+
 </script>
 
 <style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
 
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
 </style>
